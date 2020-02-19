59bf14891c48a48b12bf09bd2fbed8407b0e7880
fals3n3gativ3 <fals3n3gativ3@local.bb>
2020-02-19 13:35:18 +0100

    Read users/tags from file

--- wykoogle.py
+++ wykoogle.py
@@ -82,31 +82,50 @@ def pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron_do_analizy):
         tablica_id_wpisow += list(dict.fromkeys(temp))
     print("\t\t[+] ZAKOŃCZONO")
     return tablica_id_wpisow
+ 
+def pobranie_listy_analizowanych_tagow_i_uzytkownikow():
     
+    return [nielubiany_uz.rstrip('\n') for nielubiany_uz in  open('nielubiani_uzytkownicy', 'r')], [nielubiany_tag.rstrip('\n') for nielubiany_tag in  open('nielubiane_tagi', 'r')], [lubiany_uz.rstrip('\n') for lubiany_uz in  open('lubiani_uzytkownicy', 'r')], [lubiany_tag.rstrip('\n') for lubiany_tag in  open('lubiane_tagi', 'r')]   
 
 
 # ==== MAIN ====
 
-tablica_analizowanych_nielubianych_uzytkownikow = ['A', 'B', 'C']
-tablica_analizowanych_nielubianych_tagow = ['#X', '#Y', '#Z']
-tablica_analizowanych_lubianych_uzytkownikow = ['D', 'E', 'F']
-tablica_analizowanych_lubianych_tagow = ['#T', '#U', '#V']
-nazwa_uzytkownika = "MikolajSobczak1985"
+tablica_nielubianych_uzytkownikow = []
+tablica_nielubianych_tagow = []
+tablica_lubianych_uzytkownikow = []
+tablica_lubianych_tagow = []
+
+
 liczba_stron = 2
+nazwa_uzytkownika = "MikolajSobczak1985"
 
-surowe_dane = plusujacy_wpis_surowe_dane(id_wpisu)
-tablica_plusujacych_wpis = ekstrakcja_plusujacych_z_surowych_danych(surowe_dane)
-surowe_dane = komentujacy_wpis_surowe_dane(id_wpisu)
-komentujacy = ekstrakcja_komentujacych_wpis(surowe_dane)
-id_wpisow_uzytkownika = pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron) 
+#surowe_dane = plusujacy_wpis_surowe_dane(id_wpisu)
+#tablica_plusujacych_wpis = ekstrakcja_plusujacych_z_surowych_danych(surowe_dane)
+#surowe_dane = komentujacy_wpis_surowe_dane(id_wpisu)
+#komentujacy = ekstrakcja_komentujacych_wpis(surowe_dane)
+#id_wpisow_uzytkownika = pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron) 
 
-print("ID WPISÓW " + nazwa_uzytkownika + " z " + str(liczba_stron) + " stron:")
-print(id_wpisow_uzytkownika)
 
-print("PLUSUJĄCY:")
-print(tablica_plusujacych_wpis)
+print("LISTY ANALIZOWANYCH TAGÓW I UŻYTKOWNIKÓW:")
+tablica_nielubianych_uzytkownikow, tablica_nielubianych_tagow, tablica_lubianych_uzytkownikow, tablica_lubianych_tagow = pobranie_listy_analizowanych_tagow_i_uzytkownikow()
+print("LUBIANI UŻYTKOWNICY:")
+print(tablica_lubianych_uzytkownikow)
+print("LUBIANE TAGI:")
+print(tablica_lubianych_tagow)
+print("NIELUBIANI UŻYTKOWNICY:")
+print(tablica_nielubianych_uzytkownikow)
+print("NIELUBIANE TAGI:")
+print(tablica_nielubianych_tagow)
 
-print("KOMENTUJĄCY:")
-print(komentujacy)
+
+
+#print("ID WPISÓW " + nazwa_uzytkownika + " z " + str(liczba_stron) + " stron:")
+#print(id_wpisow_uzytkownika)
+
+#print("PLUSUJĄCY:")
+#print(tablica_plusujacych_wpis)
+
+#print("KOMENTUJĄCY:")
+#print(komentujacy)
 
 
