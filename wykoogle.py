cacfcbff9c87081dd97ae4275a2491bcb60bbb68
fals3n3gativ3 <fals3n3gativ3@local.bb>
2020-02-21 11:41:16 +0100

    Automatization

--- wykoogle.py
+++ wykoogle.py
@@ -27,7 +27,7 @@ def plusujacy_wpis_surowe_dane(id_spisu):
     print("\t\t[+] ZAKOŃCZONO")
     return surowe_dane_string.text
 
-def ekstrakcja_plusujacych_z_surowych_danych(surowe_dane_string):
+def ekstrakcja_plusujacych_wpis(surowe_dane_string):
     
     print("\tEkstrakcja plusujących...")
     try:
@@ -38,6 +38,10 @@ def ekstrakcja_plusujacych_z_surowych_danych(surowe_dane_string):
     
     print("\t\t[+] ZAKOŃCZONO")
     return tablica_plusujacych
+
+def pobranie_plusujacych_wpis(id_wpisu):
+    surowe_ddane = plusujacy_wpis_surowe_dane(id_wpisu)
+    return ekstrakcja_plusujacych_wpis(surowe_dane)
 
 def komentujacy_wpis_surowe_dane(id_wpisu):
     
@@ -66,6 +70,10 @@ def ekstrakcja_komentujacych_wpis(surowe_dane_string):
     print("\t\t[+] ZAKOŃCZONO")
     return komentujacy   
 
+def pobranie_komentujacych_wpis(id_wpisu):
+    surowe_dane = komentujacy_wpis_surowe_dane(id_wpisu)
+    return ekstrakcja_komentujacych_wpis(surowe_dane) 
+
 def pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron_do_analizy):
     #TODO
     #Stworzyć mechanizm, pobierający id wpisów do określonej daty
@@ -83,9 +91,54 @@ def pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron_do_analizy):
     print("\t\t[+] ZAKOŃCZONO")
     return tablica_id_wpisow
  
+def pobranie_id_wpisow_na_tagu(nazwa_tagu, liczba_stron_do_analizy):
+    #TODO
+    #Stworzyć mechanizm, pobierający id wpisów do określonej daty
+    tablica_id_wpisow = []
+    numer_strony = 1
+
+    print("\n\tPobieranie ID wpisów na tagu " + nazwa_uzytkownika + " z " + str(liczba_stron_do_analizy) + " pierwszych stron...")
+    for strona in range(numer_strony, liczba_stron_do_analizy+1):
+        surowe_dane_strony = requests.get("https://wykop.pl/tag/" + nazwa_uzytkownika + "/strona/" + str(numer_strony)) 
+        #TODO
+        #Tutaj też trzeba pomyśleć nad czymś sprytnieszym - xpath?
+        temp = re.findall('data-id="(\d*)" data-type="entry"', (surowe_dane_strony.text).replace('\n', ' ')) 
+        #Append kolejnych id z wielu stron 
+        tablica_id_wpisow += list(dict.fromkeys(temp))
+    print("\t\t[+] ZAKOŃCZONO")
+    return tablica_id_wpisow
+ 
+
 def pobranie_listy_analizowanych_tagow_i_uzytkownikow():
     
     return [nielubiany_uz.rstrip('\n') for nielubiany_uz in  open('nielubiani_uzytkownicy', 'r')], [nielubiany_tag.rstrip('\n') for nielubiany_tag in  open('nielubiane_tagi', 'r')], [lubiany_uz.rstrip('\n') for lubiany_uz in  open('lubiani_uzytkownicy', 'r')], [lubiany_tag.rstrip('\n') for lubiany_tag in  open('lubiane_tagi', 'r')]   
+
+
+def pobranie_komentujacych_uzytkownika(nazwa_uzytkownika, liczba_stron):
+    
+    lista_id_postow_uzytkownika = []
+    lista_komentujacych_uzytkownika = []
+    lista_id_postow_uzytkownika = pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron)
+    for id_wpisu on lista_id_postow_uzytkownika:
+        lista_komentujacych_uzytkownika += pobranie
+
+def pobranie_plusujacych_uzytkownika(nazwa_uzytkownika, liczba_stron):
+    pass
+    # j.w
+
+def pobranie_aktywnych_lubiany_uz():
+    pass
+
+def pobranie_aktywnych_nielubiany_uz():
+    pass
+
+def pobranie_aktywnych_lubiany_tag():
+    pass
+
+def pobranie_aktywnych_nielubiany_tag();
+    pass
+
+
 
 
 # ==== MAIN ====
@@ -95,37 +148,9 @@ tablica_nielubianych_tagow = []
 tablica_lubianych_uzytkownikow = []
 tablica_lubianych_tagow = []
 
-
 liczba_stron = 2
-nazwa_uzytkownika = "MikolajSobczak1985"
-
-#surowe_dane = plusujacy_wpis_surowe_dane(id_wpisu)
-#tablica_plusujacych_wpis = ekstrakcja_plusujacych_z_surowych_danych(surowe_dane)
-#surowe_dane = komentujacy_wpis_surowe_dane(id_wpisu)
-#komentujacy = ekstrakcja_komentujacych_wpis(surowe_dane)
-#id_wpisow_uzytkownika = pobranie_id_wpisow_uzytkownika(nazwa_uzytkownika, liczba_stron) 
-
-
-print("LISTY ANALIZOWANYCH TAGÓW I UŻYTKOWNIKÓW:")
 tablica_nielubianych_uzytkownikow, tablica_nielubianych_tagow, tablica_lubianych_uzytkownikow, tablica_lubianych_tagow = pobranie_listy_analizowanych_tagow_i_uzytkownikow()
-print("LUBIANI UŻYTKOWNICY:")
-print(tablica_lubianych_uzytkownikow)
-print("LUBIANE TAGI:")
-print(tablica_lubianych_tagow)
-print("NIELUBIANI UŻYTKOWNICY:")
-print(tablica_nielubianych_uzytkownikow)
-print("NIELUBIANE TAGI:")
-print(tablica_nielubianych_tagow)
 
 
-
-#print("ID WPISÓW " + nazwa_uzytkownika + " z " + str(liczba_stron) + " stron:")
-#print(id_wpisow_uzytkownika)
-
-#print("PLUSUJĄCY:")
-#print(tablica_plusujacych_wpis)
-
-#print("KOMENTUJĄCY:")
-#print(komentujacy)
 
 
